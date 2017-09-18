dfc9e0b10d0764a62b87129dee9e66e17c0d80f8
tomo-sato <dcw.tsato@gmail.com>
2017-09-18 09:39:42 +0900

    DDL修正。

--- doc/ddl/00_create_db_ddl.sql
+++ doc/ddl/00_create_db_ddl.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE `webapi_stub` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- Creation an user.for the database. 
-GRANT ALL PRIVILEGES ON webapi_stub.* TO webapi_stub@localhost IDENTIFIED BY 'webapi_stub123';
-GRANT ALL PRIVILEGES ON webapi_stub.* TO webapi_stub@'%' IDENTIFIED BY 'webapi_stub123';
+GRANT ALL PRIVILEGES ON webapi_stub.* TO webapi_stub_user@localhost IDENTIFIED BY 'webapi_stub123';
+GRANT ALL PRIVILEGES ON webapi_stub.* TO webapi_stub_user@'%' IDENTIFIED BY 'webapi_stub123';
 
 -- Reflects the all privileges as above
 FLUSH PRIVILEGES;
